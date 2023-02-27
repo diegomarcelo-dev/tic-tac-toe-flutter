fa680a481385332e119707eb6bf4b2a98749a853
Diego Marcelo <diegomarcelofs@gmail.com>
2023-02-27 13:43:19 -0300

    Arquivos do projeto

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color(0xff247fef);
+  static Color secondaryColor = Color(0xfffaeb1c);
+  static Color accentColor = Color(0xfff10732);
+
+  static Color button = Color(0xff2f205a);
+  static Color win = Color(0xff1af801);
+  static Color loss = Color(0xfffa0422);
+
+
+}
